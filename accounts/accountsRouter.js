243582c293d0395e5988cab3935238269eb18fe1
Liam Sutton <devliam90@gmail.com>
2019-11-04 13:50:21 +0000

    add create read and update endpoints

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -6,8 +6,46 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
-    const result = await db('accounts');
-    res.json(result);
+    const accounts = await db('accounts');
+    res.json(accounts);
+  } catch (error) {
+    res.status(500).json({
+      message: 'Your request could not be processed ' + error.message
+    });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const account = await db('accounts').where({ id: req.params.id });
+    res.json(account);
+  } catch (error) {
+    res.status(500).json({
+      message: 'Your request could not be processed ' + error.message
+    });
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const newAccount = await db('accounts').insert({
+      name: req.body.name,
+      budget: req.body.budget
+    });
+    res.json('New account successfully created with an id of ' + newAccount[0]);
+  } catch (error) {
+    res.status(500).json({
+      message: 'Your request could not be processed ' + error.message
+    });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedAccount = await db('accounts')
+      .where({ id: req.params.id })
+      .update({ name: req.body.name, budget: req.params.budget });
+    res.json(updatedAccount + ' Account was updated');
   } catch (error) {
     res.status(500).json({
       message: 'Your request could not be processed ' + error.message
